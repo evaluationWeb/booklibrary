db032ad6054329aa87d2f66b45b5dde2857eb001
mithridatem <mathieumithridate@adrar-formation.com>
2025-03-27 16:45:31 +0100

    feat : correction Exercice 2

--- cypress/e2e/category.cy.js
+++ cypress/e2e/category.cy.js
@@ -1,6 +1,7 @@
 describe('Test Category', () => {
-    it('Ajout category valide ', () => {
+    /* it('Ajout category valide ', () => {
         //Arrange
+        const cat = "SF"
         cy.visit('/category/new')
         //Act
         cy.get('#category_label').type("SF")
@@ -9,13 +10,57 @@ describe('Test Category', () => {
         //Assert
         cy.contains("table tr", "SF").should("exist")
     })
-
-    it('Erreur 500 doublon category', ()=> {
-         //Arrange
+    it('Ajout Erreur 500 doublon category', ()=> {
         cy.visit('/category/new')
         cy.get('#category_label').type("SF")
         cy.get('.btn').click()
         cy.wait(1000)
+        cy.get('.container > .break-long-words').contains("An exception")
+    }) */
+
+    it('Modification category valide', ()=>{
+        //Arrange
+        cy.visit('/category')
+        const category = "Nouveau"
+        const id = 1;
+        //Act
+        cy.get('[href="/category/'+ id +'/edit"]').click()
+        cy.wait(1000)
+        cy.get('#category_label').clear().type(category)
+        cy.get('[name="category"] > .btn').click()
+        //Assert
+        cy.wait(500)
+        cy.contains("table tr", category).should("exist")
+    })
+
+    it('Modification category non identique', ()=>{
+        //Arrange
+        cy.visit('/category')
+        const category = "Nouveau"
+        const id = 2;
+        //Act
+        cy.get('[href="/category/'+ id +'/edit"]').click()
+        cy.wait(1000)
+        cy.get('#category_label').clear().type(category)
+        cy.get('[name="category"] > .btn').click()
+        //Assert
+        cy.wait(500)
+        cy.contains("table tr", category).should("exist")
+    })
+
+    it('Modification category Doublon', ()=>{
+        //Arrange
+        cy.visit('/category')
+        const category = "SF"
+        const categoryDoublon = "Nouveau"
+        const id = 2;
+        //Act
+        cy.get('[href="/category/'+ id +'/edit"]').click()
+        cy.wait(1000)
+        cy.get('#category_label').clear().type(categoryDoublon)
+        cy.get('[name="category"] > .btn').click()
+        //Assert
+        cy.wait(500)
         cy.get('.container > .break-long-words').contains("An exception")
     })
   })
