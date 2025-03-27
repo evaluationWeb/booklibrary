4c551684035f92d6caea485246807afea6877ba3
mithridatem <mathieumithridate@adrar-formation.com>
2025-03-27 19:18:07 +0100

    feat : CI/CD Cypress e2e

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://127.0.0.1:8001",
+    baseUrl: "http://127.0.0.1:8001",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
